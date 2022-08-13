15b4dd30073737be10e9db31aca21eec5475ecff
Seo7829 <104804033+MTSEO1@users.noreply.github.com>
2022-08-13 23:33:33 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,20 @@
+const API_KEY = 'a8661d34cb361bc449ae3b85f9c1d91b';
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  console.log('You live in ', lat, lng);
+  console.log(lat, lng);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${Math.floor(
+        data.main.temp
+      )}℃`;
+    });
 }
 function onGeoError() {
   alert("Can't Find you. No weather for you");
